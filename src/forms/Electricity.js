e1926976d746de63aad60c54a48d9580e8dbd93a
Rakshith12345 <88425450+Rakshith12345@users.noreply.github.com>
2025-12-16 12:30:54 +0530

    Update Electricity.js

--- src/forms/Electricity.js
+++ src/forms/Electricity.js
@@ -169,7 +169,7 @@ function Electricity() {
                     type="submit"
                     fullWidth
                   >
-                   Generate bills
+                   Generate
                   </Button>
                 </Grid>
               </Grid>
